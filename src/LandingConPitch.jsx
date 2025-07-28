ced55aa07cd46b0b8fa73e69ab351d09ed1a50c8
procervantes <zoomleon@gmail.com>
2025-07-28 17:08:04 -0600

    Add files via upload

--- src/LandingConPitch.jsx
+++ src/LandingConPitch.jsx
@@ -2,9 +2,9 @@
 import React from "react";
 import emailjs from "@emailjs/browser";
 import { Card, CardContent } from "../components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { Textarea } from "../components/ui/textarea";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { Helmet } from "react-helmet";
