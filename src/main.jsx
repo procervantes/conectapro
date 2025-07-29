5c5520d5a940aa372e6b5f6de2c8eb64618343d6
procervantes <zoomleon@gmail.com>
2025-07-28 19:23:27 -0600

    borra6

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import LandingConPitch from "./LandingConPitch";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <LandingConPitch />
-  </React.StrictMode>
-);
