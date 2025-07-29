0cc364abf7afa820f998a6be8acd6f0a4b947a0f
procervantes <zoomleon@gmail.com>
2025-07-28 21:21:11 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import AdminPanel from './admin/AdminPanel';
+import React from "react";
 
-export default function App() {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/admin' element={<AdminPanel />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>ConectaPro funcionando correctamente</h1>
+    </div>
   );
 }
+
+export default App;
