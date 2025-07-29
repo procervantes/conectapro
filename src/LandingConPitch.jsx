040e89450a27ecf5c501b5eb7f62b5d133eef8f0
procervantes <zoomleon@gmail.com>
2025-07-28 19:23:11 -0600

    borra5

--- src/LandingConPitch.jsx
+++ src/LandingConPitch.jsx
@@ -1,17 +1,1 @@
 
-import React from "react";
-import { Card } from "./components/ui/card";
-import { Button } from "./components/ui/button";
-
-const LandingConPitch = () => {
-  return (
-    <div>
-      <Card>
-        <h1>Bienvenido a ConectaPro</h1>
-        <Button>Explorar</Button>
-      </Card>
-    </div>
-  );
-};
-
-export default LandingConPitch;
